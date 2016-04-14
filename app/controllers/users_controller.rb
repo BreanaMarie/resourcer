ca74899f2b5591570c396f9254c039b53101e42b
Mala <m.nimalasuriya@gmail.com>
2016-04-14 16:04:30 -0700

    updated user controller and added form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create( user_params)
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url
+    else
+      render "new"
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   has_one :profile
-  
+
+  has_secure_password
+
+  attr_accessor :email, :password, :password_confirmation
+
+  validates_uniqueness_of :email
+
+  validates :password, presence: true,
+                        length: {
+                          minimum: 6,
+                          maximum: 20
+                        }
+  validates :password_confirmation, presence: true
+
+
 end
