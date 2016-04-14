10fba0a42eb49aab0616b20e4434444c994f7f60
BreanaMarie <msbremarie@gmail.com>
2016-04-13 17:51:28 -0700

    seed data for users and profiles working.  active record migration, change phone to string on profile table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413201626) do
+ActiveRecord::Schema.define(version: 20160414003441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20160413201626) do
     t.string   "address"
     t.string   "city"
     t.string   "state"
-    t.integer  "phone"
+    t.string   "phone"
     t.string   "image"
     t.text     "bio"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,25 +15,24 @@ require 'ffaker'
   user_params = Hash.new
   user_params[:password]  = "12345"
   user_params[:email] = FFaker::Internet.email
+
   new_user = User.create(user_params)
-  p new_user
-  p new_user.id
-  puts new_user.email
+
 
   1.times do
 
     new_profile = Profile.new
-    profile_params[:firstname] = FFaker::Name.name
-    profile_params[:lastname] = FFaker::Name.name
-    profile_params[:address] = Faker::Address.street_address
-    profile_params[:city] = FFaker::Address.city
-    profile_params[:state] = FFaker::AddressUS.state
-    profile_params[:phone] = Faker::PhoneNumber.short_phone_number
-    profile_params[:image] = FFaker::Avatar.image
-    profile_params[:bio] = FFaker::Lorem.paragraph(3)
+    new_profile.firstname = FFaker::Name.name
+    new_profile.lastname = FFaker::Name.name
+    new_profile.address = FFaker::Address.street_address
+    new_profile.city = FFaker::Address.city
+    new_profile.state = FFaker::AddressUS.state
+    new_profile.phone = FFaker::PhoneNumber.short_phone_number
+    new_profile.image = FFaker::Avatar.image
+    new_profile.bio = FFaker::Lorem.paragraph(3)
 
     new_profile.save
-    new_user.profile.push new_profile
+    new_user.profile = new_profile
   end
 
 end
