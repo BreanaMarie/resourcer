3ece05f49dd480274397f370f4a0853c97fb2e30
Mala <m.nimalasuriya@gmail.com>
2016-04-14 21:23:36 -0700

    user sign up working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,50 @@
 class UsersController < ApplicationController
+
+# GET users/
   def index
     @users = User.all
     render :index
   end
 
+# GET /users/id
   def show
   end
 
+# GET /users/new
+# creates user
   def new
     @user = User.new
-    render :new
   end
 
+# POST /users
+# saves user
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      redirect_to root_url
+      redirect_to root_url, notice: "thanks!"
     else
       render "new"
     end
   end
 
+# GET /users/id/edit
+  def edit
+  end
+
+# PATCH/PUT /users/id
+  def update
+  end
+
+  # DELETE /users/id
   def destroy
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,6 @@
 class User < ActiveRecord::Base
-  has_one :profile
-
   has_secure_password
-
-  attr_accessor :email, :password, :password_confirmation
-
+  has_one :profile
+  # attr_accessor :email, :password, :password_confirmation
   validates_uniqueness_of :email
-
-  validates :password, presence: true,
-                        length: {
-                          minimum: 6,
-                          maximum: 20
-                        }
-  validates :password_confirmation, presence: true
-
-
 end
