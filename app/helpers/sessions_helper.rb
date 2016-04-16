4917b4a26f9cc1d7dac6cf6d24bef8c560cb9669
BreanaMarie <msbremarie@gmail.com>
2016-04-15 21:39:14 -0700

    define current_user add destroy sessions, add welcome feature to navigation bar and login logouts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+
+  def login(user)
+    session[:user_id] = user.id
+    @current_user = user.id
+  end
+
+  def logged_in?
+    if current_user == nil
+    else
+      true
+    end
+  end
+
+  def logout
+    @current_user = session[:user_id] = nil
+  end
+
+  def current_user 
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
+  end
 end
